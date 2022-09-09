f743f4b719b3379b4c48bb0b22773e795fa45f3d
Johnmusyomi <johnmusyomi@gmail.com>
2022-09-09 07:46:12 -0700

    Task 0 positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+/*
+ * File: 0-positive_or_negative.c
+ *
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints a random number and states whether
+ * it is a positive, negative or zero.
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+
+	return (0);
+
