6eba96ddc13d923876c3d0a8c2a1f57a7a887055
Johnmusyomi <johnmusyomi@gmail.com>
2022-09-14 13:43:56 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,7 +5,7 @@ int _putchar(char);
 void print_alphabet(void);
 int _islower(int c);
 int _isalpha(int c);
-int _print_sign(int n);
+int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
@@ -15,4 +15,5 @@ void print_to_98(int n);
 void print_times_table(int n);
 void num(int r, int n);
 void print_alphabet_x10(void);
+
 #endif
