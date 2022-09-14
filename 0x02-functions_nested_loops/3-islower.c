e2b38fd3119cb0cdf93312f193e11a1b3ef9c5be
Johnmusyomi <johnmusyomi@gmail.com>
2022-09-14 08:43:17 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ * Description: checks if a character is lowercase
+ * @c: the integer value it receives
+ * Return: 1 if true. 0 if false.
+ */
+int _islower(int c)
+{
+int j = 'a';
+
+for (j = 'a'; i <= 'z'; j++)
+{
+	/* refer int c*/
+	if (c == i)
+	{
+	Return(1);
+	}
+	{
+	return (0);
+	}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -15,5 +15,4 @@ void print_to_98(int n);
 void print_times_table(int n);
 void num(int r, int n);
 void print_alphabet_x10(void);
-
 #endif
