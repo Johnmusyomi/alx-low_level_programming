cda8896eefabb9008fa8a8ea60a07a77085ad93d
Johnmusyomi <johnmusyomi@gmail.com>
2022-09-20 16:08:42 -0700

    comply with betty nugging

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,12 +12,11 @@ int i = 0;
 
 while (str[i] != '\0')
 {
-	if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-		}
+if (i % 2 == 0)
+	{
+	_putchar(str[i]);
+	}
 	i++;
 }
-_putchar('\n');
+	_putchar('\n');
 }
- 
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * print_array - prints n elements of an array of integers, followed by a new line
+ * print_array - prints n elements of an array of integers
+ * followed by a new line
  * @a: array pointer int
  * @n: int type integer
  * Description: numbers should be separated by comma then space
