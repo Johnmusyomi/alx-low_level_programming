7458fed1f38097b2d4ca0b96d031d25248827177
Johnmusyomi <johnmusyomi@gmail.com>
2022-09-27 09:56:11 -0700

    task 6 fix

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -12,18 +12,12 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	i = 0;
-	j = 0;
-	while (i < 64)
+	for (i = 0; i < 8; i++)
 	{
-	if (i % 8 == 0 && i != 0)
-	{
-		j = i;
-		_putchar('\n');
-	}
-
-	_putchar(a[i / 8][i - j]);
-	i++;
-	}
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(*(*(i + a) + j));
+		}
 	_putchar('\n');
+	}
 }
